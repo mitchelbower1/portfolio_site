e682447329e0af1cc2a3218d6671d409ca3561d0
Mitchel Bower <mitchelbower@outlook.com>
2020-09-02 01:41:33 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainComponent} from "./main/main.component"
+import {MusicComponent} from "./music/music.component"
 
-
-const routes: Routes = [];
+const routes: Routes = [{path: "", redirectTo:"/main", pathMatch:"full"},
+{path: "main", component: MainComponent},
+{path: "music", component: MusicComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
