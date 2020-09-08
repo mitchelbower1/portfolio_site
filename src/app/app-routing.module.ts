fb4d44a399a37375974f314ce847f3c899d761bf
Mitchel Bower <mitchelbower@outlook.com>
2020-09-08 18:45:56 -0400

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {MainComponent} from "./main/main.component"
 import {MusicComponent} from "./music/music.component"
 import { BikesAndCarsComponent } from './bikes-and-cars/bikes-and-cars.component';
+import {NavigationComponent} from "./navigation/navigation.component";
 
 const routes: Routes = [{path: "", redirectTo:"/main", pathMatch:"full"},
 {path: "main", component: MainComponent},
 {path: "music", component: MusicComponent},
-{path: "bikes-and-cars", component: BikesAndCarsComponent}
+{path: "bikes-and-cars", component: BikesAndCarsComponent},
+{path: "navigation", component: NavigationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { MusicComponent } from './music/music.component';
 import { BikesAndCarsComponent } from './bikes-and-cars/bikes-and-cars.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     MusicComponent,
-    BikesAndCarsComponent
+    BikesAndCarsComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
