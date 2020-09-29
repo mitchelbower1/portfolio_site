23d90a6fbab4886eafc588e683ee9e13ced3ab55
Mitchel Bower <mitchelbower@outlook.com>
2020-09-29 08:07:15 -0400

    added service and show/ hide feature

---
+++ src/app/portfolio.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioService {
+  show:boolean = false;
+  buttonName:any = 'show';
+
+  constructor() { }
+
+  toggle() {
+    this.show = !this.show;
+    if(this.show) {
+      this.buttonName = "Hide"
+    } else { 
+      this.buttonName = "Show More"
+    }
+  }
+}
+
+
