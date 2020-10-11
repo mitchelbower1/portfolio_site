27338696117bfb0f8a7241aa56360729d56ed5c3
Mitchel Bower <mitchelbower@outlook.com>
2020-10-11 12:09:03 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -8,13 +7,15 @@ import { MusicComponent } from './music/music.component';
 import { BikesAndCarsComponent } from './bikes-and-cars/bikes-and-cars.component';
 import { NavigationComponent } from './navigation/navigation.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     MusicComponent,
     BikesAndCarsComponent,
-    NavigationComponent
+    NavigationComponent,
+
   ],
   imports: [
     BrowserModule,
