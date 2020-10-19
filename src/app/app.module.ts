c2dd00b812ab705016e7a9099613a11b82d66f7b
Mitchel Bower <mitchelbower@outlook.com>
2020-10-19 10:33:03 -0400

    added youtube component and bandcamp link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { MainComponent } from './main/main.component';
 import { MusicComponent } from './music/music.component';
 import { BikesAndCarsComponent } from './bikes-and-cars/bikes-and-cars.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import { YtPlayerAngularModule } from 'yt-player-angular';
 
 
 @NgModule({
@@ -19,7 +21,9 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    YouTubePlayerModule,
+    YtPlayerAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
