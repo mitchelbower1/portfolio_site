dda9aec41559cb1038dbd3b7950fd5bdae65fc30
Mitchel Bower <mitchelbower@outlook.com>
2020-12-08 16:09:57 -0800

    surge teardown refactor

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -7,62 +7,62 @@ import { PortfolioService } from '../portfolio.service';
   styleUrls: ['./music.component.css']
 })
 export class MusicComponent implements OnInit {
-  buttonName:any = 'Ormsby HypeGTR';
-  show:boolean = false;
-  buttonNameAgile:any = 'Agile Interceptor';
-  showAgile:boolean = false;
-  buttonNameJackson:any = 'Jackson';
-  showJackson:boolean = false;
-  buttonNameEsp:any = 'ESP B-105';
-  showEsp:boolean = false;
-  buttonNameEspFretless:any = 'ESP B-205SM (fretless)';
-  showEspFretless:boolean = false;
+  buttonName: any = 'Ormsby HypeGTR';
+  show: boolean = false;
+  buttonNameAgile: any = 'Agile Interceptor';
+  showAgile: boolean = false;
+  buttonNameJackson: any = 'Jackson';
+  showJackson: boolean = false;
+  buttonNameEsp: any = 'ESP B-105';
+  showEsp: boolean = false;
+  buttonNameEspFretless: any = 'ESP B-205SM (fretless)';
+  showEspFretless: boolean = false;
   constructor(private service: PortfolioService) { }
 
   ngOnInit(): void {
   }
-
-   toggle() {
+  // show/hide functionality
+  toggle() {
     this.show = !this.show;
-    if(this.show) {
+    if (this.show) {
       this.buttonName = "Hide"
-    } else { 
+    } else {
       this.buttonName = "Ormsby HypeGTR"
     }
   }
 
   toggleAgile() {
     this.showAgile = !this.showAgile;
-    if(this.showAgile) {
+    if (this.showAgile) {
       this.buttonNameAgile = "Hide"
-    } else { 
+    } else {
       this.buttonNameAgile = "Agile Interceptor"
     }
   }
 
   toggleJackson() {
     this.showJackson = !this.showJackson;
-    if(this.showJackson) {
+    if (this.showJackson) {
       this.buttonNameJackson = "Hide"
-    } else { 
+    } else {
       this.buttonNameJackson = "Jackson"
     }
   }
 
   toggleEsp() {
     this.showEsp = !this.showEsp;
-    if(this.showEsp) {
+    if (this.showEsp) {
       this.buttonNameEsp = "Hide"
-    } else { 
+    } else {
       this.buttonNameEsp = "ESP B-105"
     }
   }
 
   toggleEspFretless() {
     this.showEspFretless = !this.showEspFretless;
-    if(this.showEspFretless) {
+    if (this.showEspFretless) {
       this.buttonNameEspFretless = "Hide"
-    } else { 
+    } else {
       this.buttonNameEspFretless = "ESP B-205SM (Fretless)"
     }
   }
